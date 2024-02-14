7d8531c753ddc1e5b8fd5affb08cc7a44be00f0f
minutaurus <made_power@naver.com>
2024-02-14 17:47:30 +0900

    @DisplayNameGeneration, @DisplayName

--- src/test/java/com/example/junit/JUnitTest.java
+++ src/test/java/com/example/junit/JUnitTest.java
@@ -1,12 +1,18 @@
 package com.example.junit;
 
 import org.junit.jupiter.api.*;
-
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class JUnitTest {
     @Test
-    @Disabled
+    @DisplayName("스터디 만들기")
     void create(){
         System.out.println("create1");
+    }
+
+    @Test
+    @Disabled
+    void createTest(){
+        System.out.println("create2");
     }
 
     @BeforeAll // 모든 테스트가 실행되기 전 동작한다. static으로 선언해야 된다.
