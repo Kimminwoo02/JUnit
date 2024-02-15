de580b509da72bec628e3c8f92855676b20e2d9c
minutaurus <made_power@naver.com>
2024-02-15 14:55:20 +0900

    @ParameterizedTest
    원하는 매개변수를 정의하여 테스트를 할 수 있다.

--- src/test/java/com/example/junit/JUnitTest.java
+++ src/test/java/com/example/junit/JUnitTest.java
@@ -1,6 +1,8 @@
 package com.example.junit;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -33,6 +35,13 @@ public class JUnitTest {
         System.out.println("테스트 반복" + repetitionInfo.getCurrentRepetition());
         System.out.println("테스트 반복" + repetitionInfo.getTotalRepetitions());
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"날씨가", "많이" ,"추워지고","있네요"})
+    void parameterizedTest(String message){
+        System.out.println(message);
+    }
+
 
     @BeforeAll // 모든 테스트가 실행되기 전 동작한다. static으로 선언해야 된다.
     static void beforeAll(){
