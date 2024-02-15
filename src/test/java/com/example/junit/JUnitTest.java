f837436dfb6bccb61e7aed5c466180cdd377df1c
minutaurus <made_power@naver.com>
2024-02-15 14:47:15 +0900

    @Tag
    @Tag를 이용하여 테스트를 그룹화 할 수 있다.

--- src/test/java/com/example/junit/JUnitTest.java
+++ src/test/java/com/example/junit/JUnitTest.java
@@ -1,18 +1,28 @@
 package com.example.junit;
 
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class JUnitTest {
     @Test
     @DisplayName("스터디 만들기")
+    @Tag("fast")
     void create(){
         System.out.println("create1");
+        Study study = new Study();
+        assertNotNull(study);
+        assertEquals(StudyStatus.DRAFT, StudyStatus.STARTED);
     }
 
     @Test
     @Disabled
+    @Tag("slow")
     void createTest(){
-        System.out.println("create2");
+        System.out.println("create2 느린 테스트");
+
     }
 
     @BeforeAll // 모든 테스트가 실행되기 전 동작한다. static으로 선언해야 된다.
