f93504cb3f00f487d78933820b786530f552b829
minutaurus <made_power@naver.com>
2024-03-01 19:03:34 +0900

    의존성 및 순환참조 제거

--- src/test/java/com/example/junit/ArcUnitTest.java
+++ src/test/java/com/example/junit/ArcUnitTest.java
@@ -7,6 +7,8 @@ import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
 @SpringBootTest
 public class ArcUnitTest {
     @Test
@@ -17,8 +19,37 @@ public class ArcUnitTest {
                 .should().onlyBeAccessed().byClassesThat()
                         .resideInAnyPackage("..study..","..member..","..domain..");
         domainPackageRule.check(classes);
-
     }
+
+
+    @Test
+    public void Services_should_only_be_accessed_by_Controllers2(){
+        JavaClasses importedClass = new ClassFileImporter().importPackages("com.example.junit");
+
+        ArchRule myRule = noClasses().that().resideInAPackage("..domain..")
+                .should().accessClassesThat().resideInAPackage("..member.."); // 도메인 안의 어떤 클래스도
+        // 멤버 패키지 안에 있는 클래스에 접근할 수 없다.
+
+
+        memberPackageRule.check(classes);
+    }
+
+    @Test
+    public void Services_should_only_be_accessed_by_Controllers3(){
+        JavaClasses importedClass = new ClassFileImporter().importPackages("com.example.junit");
+
+        ArchRule myRule = noClasses().that().resideOutsideOfPackage("..study..")
+                .should().accessClassesThat().resideInAPackage("..study.."); // 도메인 안의 어떤 클래스도
+        // 멤버 패키지 안에 있는 클래스에 접근할 수 없다.
+
+        studyPackageRule.check(classes);
+
+        // 순환참조를 없애는 코드
+        ArchRule freeOfCycles = slices().matching("..junit.(*)..")
+                .should()/beFreeOfCycles();
+        freeOfCycles.check(classes);
+    }
+
 
 
 }
